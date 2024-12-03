ca9c153aa2dc14dc3ced89f0c11af86d9cc1b3ed
Marcelo Vanzin <vanzin@apache.org>
2024-12-02 17:28:02 -0800

    Load list of source directories from disk.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: BSD-2-Clause
+import os
+
 import app
 import config
+import model
 import util
 
 
@@ -11,6 +14,7 @@ class MainWindow(util.compile_ui("main.ui")):
         self.bConfig.clicked.connect(self.handle_config)
 
         self.cfg = cfg
+        self.artists = self._load_sources()
 
     def handle_quit(self):
         self.cfg.save()
@@ -19,3 +23,28 @@ class MainWindow(util.compile_ui("main.ui")):
     def handle_config(self):
         cfg_wnd = config.ConfigWindow(self, self.cfg)
         cfg_wnd.show()
+
+    def _load_sources(self):
+        artists = []
+        artists_by_name = {}
+
+        for path, dirs, files in os.walk(self.cfg.source_path):
+            tracks = [
+                model.Track(os.path.join(path, t))
+                for t in files
+                if os.path.splitext(t)[1] in model.TRACK_EXTS
+            ]
+            if not tracks:
+                continue
+
+            artist = tracks[0].artist
+            martist = artists_by_name.get(artist)
+            if not martist:
+                martist = model.Artist(artist)
+                artists.append(martist)
+                artists_by_name[artist] = martist
+
+            martist.add(model.Album(path, tracks))
+
+        artists.sort(key=lambda a: a.name)
+        return artists
--- src/model.py
+++ src/model.py
@@ -1,13 +1,28 @@
 # SPDX-License-Identifier: BSD-2-Clause
+import mutagen
 import util
+from mutagen.easyid3 import EasyID3
+from mutagen.easymp4 import EasyMP4Tags
+from mutagen.id3 import ID3
+from mutagen.mp4 import MP4Tags
+
+TRACK_EXTS = set([".mp3", ".m4a"])
+
+
+class Artist(object):
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add(self, album):
+        self.albums.append(album)
 
 
 class Album(object):
-    def __init__(self, path):
-        self.tracks = []
+    def __init__(self, path, tracks):
+        self.tracks = tracks
 
         # TODO:
-        # - load track list
         # - load target state
         # - method for loading metadata based on first track
 
@@ -17,10 +32,56 @@ class Track(object):
         self.path = path
         self.skip = False
 
-        # TODO:
-        # - method for loading track data
+        self._loaded = False
+        self._artist = None
+        self._album = None
+        self._title = None
+        self._duration_s = None
+        self._year = None
 
-        pass
+    def _metadata(self):
+        if self._loaded:
+            return
+
+        mf = mutagen.File(self.path, easy=True)
+        if not mf:
+            raise Exception(f"Unrecognized file {path}")
+
+        if type(mf.tags) not in [EasyID3, EasyMP4Tags]:
+            raise Exception(f"Don't know how to handle {type(mf.tags)}")
+
+        self._artist = mf.tags["artist"][0]
+        self._album = mf.tags["album"][0]
+        self._title = mf.tags["title"][0]
+        try:
+            self._year = int(mf.tags["date"][0])
+        except (KeyError, ValueError):
+            print(f"track {self.path} is missing date information")
+
+        self.duration_s = int(mf.info.length)
+
+    def cover(self):
+        tags = mutagen.File(self.path, easy=True).tags
+
+        if type(tags) == ID3:
+            for candidate in ["APIC:", "APIC:cover"]:
+                art = tags.get(candidate)
+                if art:
+                    art = art.data
+                    break
+        elif type(tags) == MP4Tags:
+            art = mf.tags.get("aART")
+            if not art:
+                art = mf.tags.get("covr")
+            if isinstance(art, list):
+                art = art[0]
+
+        return art
+
+    @property
+    def artist(self):
+        self._metadata()
+        return self._artist
 
 
 class Config(util.ConfigObj):
